2060e1891c96e7626ac6e86d501d75a007430e82
Christian Wirth <christian.wirth@oracle.com>
2023-11-24 15:05:13 +0100

    add functionality, better debugability

--- src/main/java/at/wirthi/learningspring/dm/DepartureMonitor.java
+++ src/main/java/at/wirthi/learningspring/dm/DepartureMonitor.java
@@ -7,12 +7,12 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,25 +22,52 @@ public class DepartureMonitor {
 
     public static void main(String argv[]) {
         nextDeparturesForStop("WIFI%20Linz%20AG");
+        nextDeparturesForStop("Linz%2FDonau%2C%20Taubenmarkt");
+    }
+
+    public static void errorAndTerminate(String reason, String xml) {
+        System.out.println("********************************************");
+        System.out.println(xml);
+        System.out.println("********************************************");
+        System.out.println("Error: " + reason);
+        System.out.println("********************************************");
+        System.exit(0);
     }
 
     public static List<Departure> nextDeparturesForStop(String stopName) {
-        String urlSessionActivation = "http://linzag.at/static/XML_DM_REQUEST?sessionID=0&locationServerActive=1&type_dm=any&name_dm=" + stopName + "&limit=20";
+        String urlSessionActivation = "http://linzag.at/static/XML_DM_REQUEST?sessionID=0&locationServerActive=1&type_dm=any&name_dm=" + urlencode(stopName) + "&limit=20";
+        System.out.println(urlSessionActivation);
         String first = readStringFromURL(urlSessionActivation);
         System.out.println("=======================================");
         System.out.println(first);
 
         int idxSessionStart = first.indexOf("sessionID") + 11; //including sessionID="
         int idxSessionEnd = first.indexOf("\"", idxSessionStart);
+        System.out.println("Session index: " + idxSessionStart + " " + idxSessionEnd);
+        if (idxSessionStart <= 0 || idxSessionEnd <= 0) {
+            errorAndTerminate("session not found", first);
+        }
         String sessionID = first.substring(idxSessionStart, idxSessionEnd);
+        System.out.println("Session: "+sessionID);
 
-        String urlDepRequest = "http://linzag.at/static/XML_DM_REQUEST?sessionID=" + sessionID + "&requestID=1&locationServerActive=1&type_dm=any&dmLineSelectionAll=1";
+        //&locationServerActive=1
+        String urlDepRequest = "http://linzag.at/static/XML_DM_REQUEST?sessionID=" + sessionID + "&requestID=1&type_dm=any&dmLineSelectionAll=1&command=dmNext";
         String second = readStringFromURL(urlDepRequest);
 
         System.out.println("=======================================");
         System.out.println(second);
 
-        return nextDeparturesForInput(second);
+        List<Departure> departures = nextDeparturesForInput(second);
+        System.out.println("done analyzing");
+        return departures;
+    }
+
+    private static String urlencode(String stopName) {
+        try {
+            return URLEncoder.encode(stopName,"UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     public static List<Departure> nextDeparturesForInput(String input) {
@@ -99,7 +126,7 @@ public class DepartureMonitor {
 
     private static String readStringFromURL(String urlString) {
         try {
-            URL url = new URL(urlString);
+            URL url = new URI(urlString).toURL();
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", USER_AGENT);
@@ -120,7 +147,9 @@ public class DepartureMonitor {
 
             return content.toString();
         } catch (IOException ex) {
-            return "";
+            return ex.getMessage();
+        } catch (URISyntaxException ex) {
+            return ex.getMessage();
         }
     }
 }
